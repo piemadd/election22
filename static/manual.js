65d6bdb9cf451ea771e76771e855edc57c966125
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2022-11-08 03:15:15 -0600

    state meta done!

---
+++ static/manual.js
@@ -0,0 +1,226 @@
+exports.states = {
+  01: {
+    name: 'Alabama',
+    abbr: 'AL',
+  },
+  02: {
+    name: 'Arkansas',
+    abbr: 'AR',
+  },
+  03: {
+    name: 'Does Not Exist (3)',
+    abbr: '03', 
+  },
+  04: {
+    name: 'Alabama',
+    abbr: 'AL',
+  },
+  05: {
+    name: 'Alaska',
+    abbr: 'AK',
+  },
+  06: {
+    name: 'Arizona',
+    abbr: 'AZ',
+  },
+  07: {
+    name: 'Arkansas',
+    abbr: 'AR',
+  },
+  08: {
+    name: 'California',
+    abbr: 'CA',
+  },
+  09: {
+    name: 'Colorado',
+    abbr: 'CO',
+  },
+  10: {
+    name: 'Connecticut',
+    abbr: 'CT',
+  },
+  11: {
+    name: 'Delaware',
+    abbr: 'DE',
+  },
+  12: {
+    name: 'Florida',
+    abbr: 'FL',
+  },
+  13: {
+    name: 'Georgia',
+    abbr: 'GA',
+  },
+  14: {
+    name: 'Does Not Exist (14)',
+    abbr: '14',
+  },
+  15: {
+    name: 'Hawaii',
+    abbr: 'HI',
+  },
+  16: {
+    name: 'Idaho',
+    abbr: 'ID',
+  },
+  17: {
+    name: 'Illinois',
+    abbr: 'IL',
+  },
+  18: {
+    name: 'Indiana',
+    abbr: 'IN',
+  },
+  19: {
+    name: 'Iowa',
+    abbr: 'IA',
+  },
+  20: {
+    name: 'Kansas',
+    abbr: 'KS',
+  },
+  21: {
+    name: 'Kentucky',
+    abbr: 'KY',
+  },
+  22: {
+    name: 'Louisiana',
+    abbr: 'LA',
+  },
+  23: {
+    name: 'Maine',
+    abbr: 'ME',
+  },
+  24: {
+    name: 'Maryland',
+    abbr: 'MD',
+  },
+  25: {
+    name: 'Massachusetts',
+    abbr: 'MA',
+  },
+  26: {
+    name: 'Michigan',
+    abbr: 'MI',
+  },
+  27: {
+    name: 'Minnesota',
+    abbr: 'MN',
+  },
+  28: {
+    name: 'Mississippi',
+    abbr: 'MS',
+  },
+  29: {
+    name: 'Missouri',
+    abbr: 'MO',
+  },
+  30: {
+    name: 'Montana',
+    abbr: 'MT',
+  },
+  31: {
+    name: 'Nebraska',
+    abbr: 'NE',
+  },
+  32: {
+    name: 'Nevada',
+    abbr: 'NV',
+  },
+  33: {
+    name: 'New Hampshire',
+    abbr: 'NH',
+  },
+  34: {
+    name: 'New Jersey',
+    abbr: 'NJ',
+  },
+  35: {
+    name: 'New Mexico',
+    abbr: 'NM',
+  },
+  36: {
+    name: 'New York',
+    abbr: 'NY',
+  },
+  37: {
+    name: 'North Carolina',
+    abbr: 'NC',
+  },
+  38: {
+    name: 'North Dakota',
+    abbr: 'ND',
+  },
+  39: {
+    name: 'Ohio',
+    abbr: 'OH',
+  },
+  40: {
+    name: 'Oklahoma',
+    abbr: 'OK',
+  },
+  41: {
+    name: 'Oregon',
+    abbr: 'OR',
+  },
+  42: {
+    name: 'Pennsylvania',
+    abbr: 'PA',
+  },
+  43: {
+    name: 'Does Not Exist (43)',
+    abbr: '43',
+  },
+  44: {
+    name: 'Rhode Island',
+    abbr: 'RI',
+  },
+  45: {
+    name: 'South Carolina',
+    abbr: 'SC',
+  },
+  46: {
+    name: 'South Dakota',
+    abbr: 'SD',
+  },
+  47: {
+    name: 'Tennessee',
+    abbr: 'TN',
+  },
+  48: {
+    name: 'Texas',
+    abbr: 'TX',
+  },
+  49: {
+    name: 'Utah',
+    abbr: 'UT',
+  },
+  50: {
+    name: 'Vermont',
+    abbr: 'VT',
+  },
+  51: {
+    name: 'Virginia',
+    abbr: 'VA',
+  },
+  52: {
+    name: 'Does Not Exist (52)',
+    abbr: '52',
+  },
+  53: {
+    name: 'Washington',
+    abbr: 'WA',
+  },
+  54: {
+    name: 'West Virginia',
+    abbr: 'WV',
+  },
+  55: {
+    name: 'Wisconsin',
+    abbr: 'WI',
+  },
+  56: {
+    name: 'Wyoming',
+    abbr: 'WY',
+  },
+};
